3ef62f28b7cfbb1ad24f3fbc783ffdfc73a11ebe
shoyooyo <shoyooyo@gmail.com>
2019-12-04 18:45:55 +0530

    f string formatting

--- Eight.py
+++ Eight.py
@@ -1,1 +1,17 @@
-#will be adding
+# Strings - F String Formatting type
+    # let's understand with example
+        >>> model
+        'Note5'
+        >>> gb
+        6
+        >>> inr
+        12999.99
+        >>>
+    # Syntax - f"template inside this"
+    # Example - 
+    >>> f"Redmi - {model}, {gb} GB RAM, INR {inr}"
+    'Redmi - Note5, 6 GB RAM, INR 12999.99'
+
+    # Advantage - You can use the operators learnt earlier here like below
+        >>> f"Redmi - {model.upper()}, {gb} GB RAM, INR {inr*2}"
+        'Redmi - NOTE5, 6 GB RAM, INR 25999.98'
