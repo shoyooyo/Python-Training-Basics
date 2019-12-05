38b693b12a64aeaa3f2354b425efd4b4a9f79639
shoyooyo <shoyooyo@gmail.com>
2019-12-05 16:21:00 +0530

    eleven py

---
+++ Eleven.py
@@ -0,0 +1,11 @@
+# talks about numbers
+# specifically about math operators
+# example
+    >>> num1 = 10
+    >>> num2 = 15.5
+    >>> type(num1)
+    <class 'int'>
+    >>> type(num2)
+    <class 'float'>
+    >>> num1+num2
+    25.5
